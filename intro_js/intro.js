dd20a457fc4c32d9de1df52aeedbff7110523fb0
valentino.orif <valentino.orif@gmail.com>
2025-01-14 13:12:54 +0000

    créa fichier index + script et modif dev.nix

---
+++ intro_js/intro.js
@@ -0,0 +1,9 @@
+let tiket = {
+    nomFilm: "Django",
+    prix: 20,
+    numeroSalle: 2,
+}
+let nom = "Valentino"
+let textAffichage = "bonjour " + nom + ", votre film " + tiket.nomFilm + " est en salle " + tiket.numeroSalle
+
+console.log(textAffichage)
