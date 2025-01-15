b8d8d57e11f28e1db32840bda5580f9d3c7bdb54
valentino.orif <valentino.orif@gmail.com>
2025-01-15 08:35:16 +0000

    ajout file corriger + ajout fonction message score dans script

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
+// Déclaration du tableau contenant la liste des mots proposés à l'utilisateur
 const listeMots = ['Cachalot', 'Pétunia', 'Serviette']
-let score = 0
 
-let motUtilisateur = prompt('Entrez le mot : ' + listeMots[0])
-if (motUtilisateur === listeMots[0]) {
-    score++
+let score = 0
+let nombreQuestions = listeMots.length
+
+function retournerMessageScore(score, nombreQuestions) {
+    let message = 'Votre score est de ' + score + ' sur ' + nombreQuestions
+    return message
 }
-motUtilisateur = prompt('Entrez le mot : ' + listeMots[1])
-if (motUtilisateur === listeMots[1]) {
-    score++
+//boucle de parcour du tableau
+for (let i = 0; i < listeMots.length; i++){
+    //demande de saisie de mot à l'utilisateur
+    let motUtilisateur = prompt("Entrez le mot : " + listeMots[i])
+    if (motUtilisateur === listeMots[i]){
+        score++
+    }
 }
-motUtilisateur = prompt('Entrez le mot : ' + listeMots[2])
-if (motUtilisateur === listeMots[2]) {
-    score++
-}
-console.log("Votre score est de " + score + " sur 3")
+console.log(retournerMessageScore)
